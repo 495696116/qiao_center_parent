f82341e0c8346f002d57ba83c2e0a51bf02c9914
495696116 <495696116@qq.com>
2019-01-04 11:38:40 +0800

    上传新的项目

---
+++ qiao_center_service/src/main/java/com/mapper/service/QiaoCenterServiceApplication.java
@@ -0,0 +1,14 @@
+package com.mapper.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QiaoCenterServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QiaoCenterServiceApplication.class, args);
+    }
+
+}
+
